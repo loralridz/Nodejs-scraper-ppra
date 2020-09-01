0ce74c37313d54429a89b2a7c16ac7f720fd80c0
noor rida <noorrida2000@gmail.com>
2020-09-01 08:24:33 -0700

    Adding filters

--- index.js
+++ index.js
@@ -30,8 +30,10 @@ app.get("/scraptenders", async(req, res) => {
 
     // loop through array objects
     result.forEach(async(tender) => {
+        //check status
+        status = checkStatus(getDate(tender.Advertise_date), getDate(tender.Close_date));
         //create row in db 
-        await createtender(tender.No, tender.Details, tender.Advertise_date, tender.Close_date, tender.Document);
+        await createtender(tender.No, tender.Detail, getDate(tender.Advertise_date), getDate(tender.Close_date), tender.Document, status);
 
     });
     //Success response
@@ -39,7 +41,27 @@ app.get("/scraptenders", async(req, res) => {
 
 })
 
+
+// Active tenders
+app.get("/activetenders", async(req, res) => {
+    const rows = await activetenders();
+    //notify browser and send all objects
+    res.setHeader("content-type", "application/json");
+    res.send(JSON.stringify(rows));
+})
+
+// Expired tenders
+app.get("/expiredtenders", async(req, res) => {
+    const rows = await expiredtenders();
+    //notify browser and send all objects
+    res.setHeader("content-type", "application/json");
+    res.send(JSON.stringify(rows));
+})
+
+// New tenders
+// Applied tenders
 //connecting to server
+
 app.listen(5000, () => {
     console.log("server working...")
 })
@@ -80,4 +102,60 @@ async function createtender(no, detail, ad, cd, doc) {
             }
 
         });
+}
+
+// check until today active tenders
+
+async function activetenders() {
+
+    try {
+        const results = await pool.query("SELECT * FROM tender  WHERE closing_date>='2020-8-29 00:00:00' ");
+        return results.rows;
+    } catch (e) {
+        return e;
+    }
+}
+
+
+// check expired tenders
+var curday = function(sp) {
+    today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //As January is 0.
+    var yyyy = today.getFullYear();
+
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+    return (dd + sp + mm + sp + yyyy);
+};
+async function expiredtenders() {
+    varr = curday('-');
+    console.log(varr);
+    try {
+        const results = await pool.query("SELECT * FROM tender  WHERE closing_date <= varr ");
+        return results.rows;
+    } catch (e) {
+        return e;
+    }
+}
+
+function replaceAll(string, search, replace) {
+    return string.split(search).join(replace);
+}
+
+function getDate(date) {
+
+    const pieces = (date.replace(/(\r\n|\n|\r)/gm, " ")).split(" ");
+    console.log("pieces" + pieces);
+    const r = pieces[0].split("/");
+    const rm = [r[0], r[1], r[2]];
+    console.log("rm" + rm);
+    var str2 = replaceAll(rm.toString(), ",", "-");
+    if (pieces.length == 1) {
+        return str2;
+    } else {
+        console.log((str2 + " " + pieces[1].toString() + " " + pieces[2]));
+        return (str2 + " " + pieces[1].toString() + " " + pieces[2]);
+    }
+
 }
